aaf5862981f4f28b8fcd9b46ba5af02b179b052a
Lucas Loiseau <lucas.loiseau@student.ecp.fr>
2018-03-05 09:32:13 +0100

    Ajout des premières classes

---
+++ src/partOne/Bike.java
@@ -0,0 +1,19 @@
+package partOne;
+
+public abstract class Bike {
+	
+	//Bike ID
+	protected int bikeID;
+	
+	//Number which will be used to create the ID
+	protected static int counter = 100000;
+	
+	//Last digit of the ID, representing the bike
+	private static final int lastDigit = 2;
+	
+	Bike(){
+		this.bikeID = 10*counter+lastDigit;
+		counter ++;
+	}
+	
+}
---
+++ src/partOne/ElectricBike.java
@@ -0,0 +1,15 @@
+package partOne;
+
+public class ElectricBike extends Bike{
+	
+	//Constructor
+	ElectricBike(){
+		super();
+	}
+	
+	//toString method
+	public String toString() {
+		return "Vélo électrique n° "+this.bikeID;
+	}
+	
+}
