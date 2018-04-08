85a51980746e795d8eaac844dd5f5c4eb6b0aeb0
Lucas Loiseau <lucas.loiseau@student.ecp.fr>
2018-04-08 18:40:50 +0200

    le template à remplir ensuite pour les commandes

--- src/CLUI/CLUI.java
+++ src/CLUI/CLUI.java
@@ -1,5 +1,76 @@
 package CLUI;
 
-public class CLUI {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class CLUI {
+	
+	public final static void run() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Bienvenue dans l'interface myVelib");
+		String str = sc.nextLine();
+		String values[] = str.split(" ");
+		System.out.println(Arrays.deepToString(values));
+		
+		if (values[0] == "setup" ) {
+			if (values.length == 2 ) {
+				
+			}
+			else if (values.length == 6) {
+				
+			}
+			else {
+				
+			}
+		}
+		
+		else if (values[0] == "addUser") {
+			if (values.length == 4) {
+				
+			}
+			else {
+				
+			}
+		}
+		
+		else if (values[0] == "offline") {
+			
+		}
+		
+		else if (values[0] == "online") {
+			
+		}
+		
+		else if (values[0] == "rentBike") {
+			
+		}
+		
+		else if (values[0] == "returnBike") {
+			
+		}
+		
+		else if (values[0] == "displayStation") {
+			
+		}
+		
+		else if (values[0] == "displayUser") {
+			
+		}
+		
+		else if (values[0] == "sortStation") {
+			
+		}
+		
+		else if (values[0] == "display") {
+			
+		}
+		
+		else {
+			System.out.println("\""+str+"\" : "+"commande inconnue");
+		}
+	}
+
+	public static void main(String[] args) {
+		run();
+	}
 }
